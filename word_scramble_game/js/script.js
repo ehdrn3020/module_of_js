5a9ac9f33c6d2acd6c4a57415422592475e1c425
ehdrn3020 <ehdrnsla3020@naver.com>
2022-11-02 08:25:46 +0900

    20221102 add check answer & timeout

--- word_scramble_game/js/script.js
+++ word_scramble_game/js/script.js
@@ -1,14 +1,53 @@
-/* need
-1. time left
-2. random quiz & hint -> word.js
-3. check resolve and quiz
-4. refresh button
-5. send solution button
-*/
+const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint span");
+const refreshBtn = document.querySelector(".refresh-word");
+const checkBtn = document.querySelector(".check-word");
+const inputField = document.querySelector("input");
+const timeText = document.querySelector(".time b");
+
+let correctWord, timer;
+
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(()=>{
+        if(maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        } 
+        clearInterval(timer);
+        alert(`시간을 초과하셨습니다. 정답은 '${correctWord}' 입니다.`);
+        initGame();
+    }, 1000);
+}
 
 const initGame = () => {
+    initTimer(10);
+    // suffle word array
     let randomObj = words[Math.floor(Math.random() * words.length)];
-    console.log(randomObj);
-    console.log(Math.random());
+    let wordArray = randomObj.word.split("");
+    let suffle = false;
+    let orgStr = wordArray.join('');
+    while(suffle===false) {
+        wordArray.sort(() => Math.random() - 0.5);
+        if (orgStr !== wordArray.join('')) suffle=true;
+    }
+    // set value
+    wordText.innerText =  wordArray.join('');
+    hintText.innerText = randomObj.hint;
+    correctWord = orgStr;
+    inputField.value = '';
+    inputField.setAttribute("maxlength", correctWord.length);
 }
+
+const checkWord = () => {
+    let userWord = inputField.value;
+    console.log(correctWord,"//",userWord);
+    if (!userWord) return alert("정답을 입력해주세요.");
+    if (correctWord !== userWord) return alert(`'${userWord}', 정답이 아닙니다.`); 
+    alert(`'${userWord}', 정답입니다!`);
+    initGame();
+}
+
 initGame();
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord);
