b142052360340f00f32aba1e7aaebad6a36c1322
ehdrn3020 <ehdrnsla3020@naver.com>
2022-12-01 07:25:31 +0900

    20221201 add drag mouse event

---
+++ drag_image_slider/script.js
@@ -0,0 +1,27 @@
+const carousel = document.querySelector(".carousel");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    console.log(prevPageX);
+    prevScrollLeft = carousel.scrollLeft;
+    console.log(prevScrollLeft);
+}
+
+const dragging = (e) => {
+    // console.log(e.pageX);
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
