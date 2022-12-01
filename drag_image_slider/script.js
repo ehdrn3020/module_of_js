45222c392cfbf62ee6aefb85d8cc05e12729005a
ehdrn3020 <ehdrnsla3020@naver.com>
2022-12-02 07:04:31 +0900

    add <i> function & mobile touches

--- drag_image_slider/script.js
+++ drag_image_slider/script.js
@@ -1,27 +1,52 @@
 const carousel = document.querySelector(".carousel");
+const arrowIcons = document.querySelectorAll(".wrapper i");
+const firstImg = document.querySelectorAll("img")[0]
 
 let isDragStart = false, prevPageX, prevScrollLeft;
 
+// hide <i> button
+const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+// <i> button click
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", ()=> {
+        let firstImgWidth = firstImg.clientWidth + 14;
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        showHideIcons();
+    })
+})
+
 const dragStart = (e) => {
     isDragStart = true;
-    prevPageX = e.pageX;
-    console.log(prevPageX);
+    prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
-    console.log(prevScrollLeft);
 }
 
 const dragging = (e) => {
-    // console.log(e.pageX);
     if(!isDragStart) return;
     e.preventDefault();
-    let positionDiff = e.pageX - prevPageX;
+    carousel.classList.add("dragging");
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
 }
 
 const dragStop = () => {
     isDragStart = false;
+    carousel.classList.remove("dragging");
 }
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+
+
+//mobile
+carousel.addEventListener("touchstart", dragStart);
+carousel.addEventListener("touchmove", dragging);
+carousel.addEventListener("touchend", dragStop);
