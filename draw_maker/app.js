1ab3a8bb4205d262ef11f40d41fd15f1a8f39235
ehdrn3020 <ehdrnsla3020@naver.com>
2022-12-07 22:12:56 +0900

    20221207 make house using canvas

--- draw_maker/app.js
+++ draw_maker/app.js
@@ -3,14 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(0, 0, 100, 100);
-ctx.rect(50, 50, 100, 100);
-ctx.stroke();
-ctx.rect(150, 150, 100, 100);
-ctx.fill();
-
-// 이전 경로와 독립적인 path설정
-ctx.beginPath();
-ctx.rect(200, 200, 100, 100);
-ctx.fillStyle = "red";
+// 벽
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2;
+ctx.strokeRect(300, 300, 50, 100);
+// 천장
+ctx.fillRect(200, 200, 200, 20);
+// 지붕
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
 ctx.fill();
